ff01272ab33446a99cad3df14e899b9c795cf4fc
Connie Chan <conniec321@gmail.com>
2022-06-14 10:41:23 -0400

    book model created and seeded

---
+++ server/db/models/Book.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+const axios = require('axios');
+
+const Book = db.define('book', {
+    title: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.TEXT,
+        defaultValue: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7L3wF7n8RofIc0WkSQOiZU6M5zATh7kYbZw&usqp=CAU'
+    },
+    price: {
+        type: Sequelize.DECIMAL(15,6),
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT
+    }
+  }) 
+
+  module.exports = Book
+
